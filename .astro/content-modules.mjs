22d6df920bc2ddb9129453df6d59afe6eb2bc669
Chris Earl <christopherjarronearl@gmail.com>
2026-08-09 16:56:57 -0400

    Deleted leftover image folder in blog folder

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,1 @@
-
-export default new Map([
-["src/content/blog/designing-for-slow-connections.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdesigning-for-slow-connections.mdx&astroContentModuleFlag=true")],
-["src/content/blog/pricing-a-small-agency.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpricing-a-small-agency.mdx&astroContentModuleFlag=true")],
-["src/content/blog/running-discovery-workshops.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Frunning-discovery-workshops.mdx&astroContentModuleFlag=true")],
-["src/content/blog/seo-audits-we-actually-run.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fseo-audits-we-actually-run.mdx&astroContentModuleFlag=true")],
-["src/content/blog/why-astro-for-agency-sites.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwhy-astro-for-agency-sites.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
